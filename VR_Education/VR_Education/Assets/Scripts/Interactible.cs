6e85ff0f64820ef874b70e11fb63a1aa3f50836b
Burak Torun <burakttorun@hotmail.com>
2021-01-21 23:13:23 +0300

    Vr lesson basic project has been added. :tada:

---
+++ VR_Education/VR_Education/Assets/Scripts/Interactible.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    Camera cam;
+    public Material highlightedMaterial;
+    Material defaultMaterial;
+    bool selectedOnce;
+    Quaternion defRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultMaterial = GetComponent<Renderer>().material;
+        cam = Camera.main;
+    }
+
+
+    internal void Select()
+    {
+
+        GetComponent<Renderer>().material = highlightedMaterial;
+        if (!selectedOnce)
+        {
+            defRotation = transform.rotation;
+        }
+        selectedOnce = true;
+    }
+    internal void DeSelect()
+    {
+        GetComponent<Renderer>().material = defaultMaterial;
+    }
+    float zCoor;
+    Vector3 offset;
+    internal void ComputeInitialOffset()
+    {
+        
+        zCoor = cam.WorldToScreenPoint(transform.position).z; // ekran iki boyutlu olduğu için z ekseni yoktur, erişmek için kullanılan kısım.
+        offset = transform.position - GetMousePositionIn3D();
+    }
+
+    private Vector3 GetMousePositionIn3D()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = zCoor;
+
+        return cam.ScreenToWorldPoint(mousePos);
+    }
+
+    public void Translate()
+    {
+        transform.position = offset + GetMousePositionIn3D();
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = defRotation;
+    }
+
+    public void RotateInZ()
+    {
+        transform.Rotate(Vector3.forward * angle * Time.deltaTime, Space.World);
+    }
+
+    public void RotateInY()
+    {
+        transform.Rotate(Vector3.up * angle * Time.deltaTime, Space.World);
+    }
+    float angle = 120;
+    public void RotateInX()
+    {
+        transform.Rotate(Vector3.right * angle * Time.deltaTime, Space.World);
+    }
+
+    public void ScaleUp()
+    {
+        transform.localScale += new Vector3(1,1,1)*0.2f;
+        var clampX = Mathf.Clamp(transform.localScale.x, 0.1f, 1.8f);//clamp verilen değeri verilen (min max) rakamlar arasında tutmak için kullanılır.
+        var clampY = Mathf.Clamp(transform.localScale.y, 0.1f, 1.8f);
+        var clampZ = Mathf.Clamp(transform.localScale.z, 0.1f, 1.8f);
+        transform.localScale = new Vector3(clampX, clampY, clampZ);
+    }
+
+    public void ScaleDown()
+    {
+        transform.localScale -= new Vector3(1, 1, 1) * 0.2f;
+        var clampX = Mathf.Clamp(transform.localScale.x, 0.1f, 1.8f);//clamp verilen değeri verilen (min max) rakamlar arasında tutmak için kullanılır.
+        var clampY = Mathf.Clamp(transform.localScale.y, 0.1f, 1.8f);
+        var clampZ = Mathf.Clamp(transform.localScale.z, 0.1f, 1.8f);
+        transform.localScale = new Vector3(clampX, clampY, clampZ);
+    }
+}
---
+++ VR_Education/VR_Education/Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionManager : MonoBehaviour
+{
+    Camera cam;
+    public Transform cursor;
+    public Text textinfo;
+    Interactible current;
+    List<Interactible> Interactibles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        Interactibles = new List<Interactible>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var i in Interactibles)
+        {
+            i.DeSelect();
+        }
+        Interactibles.Clear();
+
+        Ray ray = cam.ScreenPointToRay(cursor.position);
+        if (Physics.Raycast(ray, out RaycastHit info))
+        {
+            Interactible interactible = info.transform.GetComponent<Interactible>();
+            if (interactible!=null)
+            {
+                interactible.Select();
+                current = interactible;
+                textinfo.text = interactible.name;
+                var offset = Vector3.up * 30;
+                textinfo.transform.position = cursor.position-offset;
+                
+
+                Interactibles.Add(interactible);
+
+                if (Input.GetKeyDown(KeyCode.Mouse0)) interactible.ComputeInitialOffset();
+                if (Input.GetKey(KeyCode.Mouse0)) interactible.Translate();
+                if (Input.GetAxis("Mouse ScrollWheel") > 0) interactible.ScaleUp();
+                if (Input.GetAxis("Mouse ScrollWheel") < 0) interactible.ScaleDown();
+                if (Input.GetKey(KeyCode.X)) interactible.RotateInX();
+                if (Input.GetKey(KeyCode.Y)) interactible.RotateInY();
+                if (Input.GetKey(KeyCode.Z)) interactible.RotateInZ();
+                if (Input.GetKey(KeyCode.R)) interactible.ResetRotation();
+
+
+
+
+
+
+            }
+        }
+        else
+        {
+            foreach (var interactible in Interactibles)
+            {
+                textinfo.text = "";
+                interactible.DeSelect();
+            }
+            Interactibles.Clear();
+        }
+
+
+    }
+}
