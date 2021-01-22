8bea99929eaab5ed4dbeeb3c776592d57e479a12
Burak Torun <burakttorun@hotmail.com>
2021-01-22 23:43:11 +0300

    We created UI and selected the wall color with the materials taken from here.

---
+++ VR_Education/VR_Education/Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    private GameObject room;
+    Material[] materials;
+    public Material material;
+    void Start()
+    {
+        room = GameObject.Find("room");
+        materials = room.GetComponent<MeshRenderer>().sharedMaterials;
+
+    }
+
+    // Update is called once per frame
+    public void SetMaterial()
+    {
+        materials[0] = material;
+        room.GetComponent<MeshRenderer>().sharedMaterials[0] = materials[0];
+        room.GetComponent<MeshRenderer>().sharedMaterials = materials;
+
+
+    }
+}
